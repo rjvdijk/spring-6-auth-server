ffb6445e02329f559de52cab15b87d32c36a9f6f
Rob van Dijk <rob@optimust.nl>
2024-02-06 16:26:34 +0100

    Initial Project

---
+++ src/test/java/guru/springframework/spring6authserver/Spring6AuthServerApplicationTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Spring6AuthServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
